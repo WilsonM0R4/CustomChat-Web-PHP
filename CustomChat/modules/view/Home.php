8a68302fc2952e1d104184839b3c05544c93d3cc
Wilson Mora <wilsonfermora19.97@gmail.com>
2017-02-21 18:23:52 -0500

    improvements in home page styles

--- CustomChat/modules/view/Home.php
+++ CustomChat/modules/view/Home.php
@@ -15,7 +15,25 @@
 
 	<div id="main_header">
 		<div id="main_header_title">CustomChat!</div>
-		<div id="plus_div">+</div>
+		<div id="plus_div">
+
+		<!-- menu list structure -->
+
+			<nav class="menu_list">
+				<ul class="menu_list_class">
+					<li class="list_title"> + 
+						<ul>
+							<div id ="list_elements">						
+							<li class="list_item">Contacts</li>
+							<li class="list_item">Your profile</li>
+							<li class="list_item">Settings</li>
+							<li class="list_item">Sign out</li>
+							</div>
+						</ul>
+					</li>
+				</ul>
+			</nav>	
+		</div>
 	</div>
 
 	<div id="recent_chats">
@@ -23,9 +41,18 @@
 	</div>
 
 	<div id="floating_online_container">
-		<input type="submit" id="online_submit" name="online_submit" value="Online"/>
+		<!--div id="online_title"></div-->
+		Online
+		<nav class="online_nav">
 		
-
+			<ul class="online_list">
+				<li>Contact 1</li>
+				<li>Contact 2</li>
+				<li>Contact 3</li>
+				<li>Contact 4</li>
+				<li>Contact 4</li>
+			</ul>
+		</nav>
 	</div>
 
 	<!--div id="shadow_test">
